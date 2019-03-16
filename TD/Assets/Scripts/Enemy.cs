0af591f5e4a6f48f0f9bfc4f86cce39ad3b079a1
MrAtkens <r.kaliaskar@mail.ru>
2019-03-16 22:41:14 +0600

    HomeWork

---
+++ TD/Assets/Scripts/Enemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Enemy : MonoBehaviour
+{
+    public static int healthPoint;
+
+    public static int startHealtPoint;
+    public static int moneyForKill;
+
+   [SerializeField]
+    private Image indicator;
+
+    [SerializeField]
+    private int damageForBase;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private int healtForEnemy;
+
+    [SerializeField]
+    private int moneyForEnemy;
+    private Transform targetPoint;
+
+    private int pointIndex=0;
+
+    private void Start()
+    {
+        startHealtPoint = healtForEnemy;
+        healthPoint = startHealtPoint;
+        targetPoint = Points.points[0];
+        moneyForKill = moneyForEnemy;
+    }
+
+    private void Update()
+    {
+       transform.position=Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
+
+        var indicatorWidth= healthPoint/startHealtPoint;
+                var indicatorScale= indicator.transform.localScale;
+                indicator.transform.localScale =new Vector3(indicatorWidth,indicatorScale.y,indicatorScale.z);
+
+         var distance=Vector3.Distance(transform.position, targetPoint.position);
+
+        if(distance <= 0.4f){
+            GetNextPoint();
+        }
+    }
+
+    private void GetNextPoint(){
+        if(pointIndex >= Points.points.Length-1){
+            Destroy(gameObject);
+            PlayerStats.Lifes -= damageForBase;
+            return;
+
+        }
+        pointIndex++;
+        targetPoint=Points.points[pointIndex];
+    }
+}
---
+++ TD/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Transform enemyPrefab;
+    [SerializeField]
+    private Transform enemyPrefabSecond;
+
+    [SerializeField]
+    private Transform enemyPrefabThrist;
+
+    [SerializeField]
+    private Transform enemyPrefabFourth;
+
+    [SerializeField]
+    private Transform spawnPoint;
+
+    [SerializeField]
+    private float timeBetweenWaves;
+    
+    private float countdownTimer;
+    
+    private int waveNumber=1;
+
+    [SerializeField]
+    private Text countdownText;
+    void Start()
+    {
+        countdownTimer=timeBetweenWaves;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countdownTimer <= 0){
+            StartCoroutine(SpawnWave());
+            countdownTimer = timeBetweenWaves;
+        }
+
+        countdownTimer -= Time.deltaTime;
+        countdownText.text= countdownTimer.ToString("0");
+    }
+    
+    private IEnumerator SpawnWave(){
+        waveNumber++;
+        for(int i = 0; i < waveNumber; i++){
+            SpawnEnemy();
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+    private void SpawnEnemy(){
+        int random = RandomShuffle();
+        if(random<5){
+        // создаём противника в определённой точке на карте 
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+        else if(random>=5 && random<7){
+        Instantiate(enemyPrefabSecond, spawnPoint.position, spawnPoint.rotation); 
+        }
+        else if(random>=7 && random<=9){
+        Instantiate(enemyPrefabThrist, spawnPoint.position, spawnPoint.rotation); 
+        }
+        else if(random == 10){
+        Instantiate(enemyPrefabFourth, spawnPoint.position, spawnPoint.rotation); 
+        }
+    }
+    private int RandomShuffle(){
+        int randomBuffer = Random.Range(0,11);
+        return randomBuffer;
+    }
+
+}
